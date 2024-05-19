1c6a60b6927b7bee8503c21c555421bcfe2f332f
Rotem Azarzar <rotemekko@gmail.com>
2024-05-19 13:31:44 +0300

    Add project files.

---
+++ bookStore project/DTO's/UpdateAccountDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookStore_project.DTO_s
+{
+    public class UpdateAccountDTO
+    {
+     
+        public string FirstName { get; set; }
+    
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ bookStore project/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace bookStore_project.Models
+{
+    public class UserModel : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        [Range(0, 100)]
+        public int? Discount { get; set; } = 0;
+
+        public List<BookModel>? Books { get; set; }
+    }
+}
